1e539a31dff975d2c289da0d8fa1663c01301f23
Anup Singh <anup.singh@entiresoftware.com>
2022-01-31 23:32:49 +0530

    some

--- client_buy/src/App.js
+++ client_buy/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello Anup</h1>
+      <h1>hello Thakur Anup Singh</h1>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
