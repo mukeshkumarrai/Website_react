3623789b86aed2dc76e0578017efe4402dd8cca2
Anup Singh <anup.singh@entiresoftware.com>
2022-02-08 21:44:33 +0530

    router

--- client_buy/src/App.js
+++ client_buy/src/App.js
@@ -9,8 +9,8 @@ import Contact from './Component/Contact';
 const App = () => {
   return (
     <>
-    <Navbar />
      <Routes>
+    <Navbar />
         <Route path="/" element={ <Home/> } />
         <Route path ="about" element={<About/>}/>
         <Route path ="contact" element ={<Contact/>} />
